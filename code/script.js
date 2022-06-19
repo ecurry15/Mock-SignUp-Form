6bf41f80c34dbe343ed359fcfbc1a367085fc6b6
Edward <Ecampbell3015@gmail.com>
2022-06-19 16:00:19 -0700

    Add password and confirm password validity

--- code/script.js
+++ code/script.js
@@ -1,11 +1,32 @@
 const phone = document.getElementById("phone");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+let passwordValue = '';
+
 phone.addEventListener('keyup', function() {
   let phoneValue = '';
   phoneValue = phone.value;
-  console.log(phoneValue);
   if (/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/.test(phoneValue)) {
     phone.setCustomValidity("");
   } else {
     phone.setCustomValidity("please type a 10 digit number Ex.000-000-0000");
   }
 })
+
+password.addEventListener('keyup', function() {
+passwordValue = password.value;
+if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(passwordValue)) {
+  password.setCustomValidity("");
+} else {
+  password.setCustomValidity("password must be 8 characters long and include numbers and letters");
+}
+});
+
+confirmPassword.addEventListener('keyup', function() {
+let cPassword = confirmPassword.value;
+if (cPassword != passwordValue) {
+  confirmPassword.setCustomValidity("Passwords do not match");
+} else {
+  confirmPassword.setCustomValidity("");
+}
+});
